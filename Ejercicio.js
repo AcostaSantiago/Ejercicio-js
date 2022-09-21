e889d6687701ee30f2e38787bdd1d6957c0d9cac
SantiagoAcosta <97812666+acostaxley@users.noreply.github.com>
2022-09-20 21:54:56 -0300

    Ejercicio.js

---
+++ Ejercicio.js
@@ -0,0 +1,25 @@
+// Tu nombre (string)
+const lista = ["Santiago"]
+console.log(lista)
+
+// Tu edad (number)
+const lista2 = [18]
+console.log(lista2)
+
+// ¿Eres desarrollador? (boolean)
+const lista3 =  [true,]
+console.log(lista3)
+
+//Tu fecha de nacimiento (Date)
+const fecha_cadena = new Date("August 18 2004");
+console.log(fecha_cadena)
+
+//Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const libro = {
+    titulo: "Piensa y hazte rico",
+    autor: "Napoleon Hill",
+    fecha: 1937,
+    url:"https://www.economicas.unsa.edu.ar/afinan/informacion_general/book/piense_y_hagase_rico.pdf",
+    }
+    console.log(libro.url)
